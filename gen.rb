6242c766720bc1b5b90011538a6937218394993e
Icaro Martins <icarofre99@gmail.com>
2020-05-13 03:46:39 -0300

    criada lista de condicionais para realizar as ações;

--- gen.rb
+++ gen.rb
@@ -116,6 +116,16 @@ def ler_json(arquivo)
 	hash
 end
 
+# \/ checar se chaves do json existem;
+def if_keys(json, key1, key2, key3)
+	return (json.has_key?(key1) && json[key1].has_key?(key2) && json[key1][key2].has_key?(key3))
+end
+
+# \/ checar se chaves do json existem;
+#~ def if_keys(json, keys)
+	#~ return (json.has_key?(keys[0]) && json[keys[0]].has_key?(keys[1]) && json[keys[0]][keys[1]].has_key?(keys[2]))
+#~ end
+
 # \/ ler arquivo JSON a partir da linha de comando;
 def ler_arquivo_json
 	if ARGV.length != 1
@@ -145,4 +155,53 @@ puts( json['main_item']['stats']['c'] )
 =end
 
 json = ler_json('D:\\Documentos\\gen-rb\\json-entrada.json')
-puts json['actions']
+#~ puts json['actions']['sub']
+
+
+if if_keys(json, 'actions', 'sub', 'arquivo') && if_keys(json, 'actions', 'sub', 'hash_subs')
+#    subs_arquivo(json['actions']['sub']['arquivo'], json['actions']['sub']['arquivo']['hash_subs'])	
+end
+
+if if_keys(json, 'actions', 'create', 'nome') && if_keys(json, 'actions', 'create', 'conteudo')
+#    criar_arquivo(json['actions']['create']['nome'], json['actions']['create']['arquivo']['conteudo'])	
+end
+
+if if_keys(json, 'actions', 'del', 'nome')
+#    deletar_arquivo(json['actions']['del']['nome'])	
+end
+
+if if_keys(json, 'actions', 'rename', 'nome') && if_keys(json, 'actions', 'rename', 'novo_nome')
+#    renomear_arquivo(json['actions']['del']['nome'], json['actions']['del']['novo_nome'])	
+end
+
+if if_keys(json, 'actions', "substituicoes-projeto", "src") && if_keys(json, 'actions', "substituicoes-projeto", 'hash_subs')
+
+end
+
+if if_keys(json, 'actions', "substituicoes-src", "src") && if_keys(json, 'actions', "substituicoes-src", "hash_subs")
+
+end
+
+if if_keys(json, 'actions', "substituir_linha_arquivo", "nome") && if_keys(json, 'actions', "substituir_linha_arquivo", "linha") && if_keys(json, 'actions', "substituir_linha_arquivo", "novo_conteudo")
+   
+end
+
+if if_keys(json, 'actions', "criar_pasta", "nome")
+
+end
+
+if if_keys(json, 'actions', "add_conteudo_arquivo", "arquivo") && if_keys(json, 'actions', "add_conteudo_arquivo", "conteudo")
+
+end
+
+if if_keys(json, 'actions', "add_linhas_arquivo", "nome") && if_keys(json, 'actions', "add_linhas_arquivo", "linha") && if_keys(json, 'actions', "add_linhas_arquivo", "array_novos_conteudos")
+
+end
+
+if if_keys(json, 'actions', "criar_diretorios_aninhados", "dir_raiz") && if_keys(json, 'actions', "criar_diretorios_aninhados", "dir_criar")
+
+end
+
+if if_keys(json, 'actions', "criar_arquivo_dir_aninhados", "dir_raiz") && if_keys(json, 'actions', "criar_arquivo_dir_aninhados", "dir_criar")
+
+end
